0916f74dbac2b4aaf7155edaf23e566f8aa957bc
Docteur-Jemper <62062343+Docteur-Jemper@users.noreply.github.com>
2023-12-12 22:04:13 +0100

    Avancer sur le Colis (avec erreur)

--- src/main/java/Classes/Colis.java
+++ src/main/java/Classes/Colis.java
@@ -1,7 +1,9 @@
 package Classes;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Colis {
 
@@ -11,9 +13,26 @@ public class Colis {
         produits = new ArrayList<>();
     }
 
+    public void Empiler(int tailleVolume, Produit produit){
+
+        produits.add(produit);
+        //produits.sort(Comparator.comparingInt(produit -> Character.getNumericValue(produit.getNom().charAt(1))));
+
+    }
+
     // méthode permettant de ranger du plus grand volume au plus petit tous les Produits dans le Colis
-    public void Empiler(){
-        // a rediger
+    public void Assambler(Entrepot entrepot){
+        Scanner scanner = new Scanner(System.in);
+        String chaineProduits = scanner.nextLine();
+
+        String[] tableauProduits = chaineProduits.replaceAll("\\s", "").split(",");
+
+        for(String nomProduit : tableauProduits){
+            int tailleVolume = Character.getNumericValue(nomProduit.charAt(1));
+            Produit produit = entrepot.recupererProduit(nomProduit);
+
+            Empiler(tailleVolume, produit);
+        }
     }
 
     public List<Produit> getContenuColis() {
